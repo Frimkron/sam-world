5d9adfa8757a86369b44759c12f9b70eedb3e6e0
mark <mfrimston@gmail.com>
2015-03-15 18:50:00 +0000

    animated sam, added title and game over

--- main.py
+++ main.py
@@ -46,30 +46,47 @@ def draw_tiles(screen, position, level, tile_graphics, tile_size):
             if tile_graphic is not None:
                 draw_pos = top_left_draw_pos[0]+i*tile_size, top_left_draw_pos[1]+j*tile_size
                 screen.blit(tile_graphic, draw_pos)
+                
+           
+def draw_sam(screen, idle_graphic, run_graphics, jump_graphic, offset, position, velocity, landed, tile_size):
+    if not landed:
+        graphic = jump_graphic
+    elif velocity[0] == 0.0:
+        graphic = idle_graphic
+    else:
+        num = len(run_graphics)
+        graphic = run_graphics[int(position[0]//(tile_size/num)) % num]
+        
+    if velocity[0] < 0.0:
+        graphic = pygame.transform.flip(graphic, True, False)
+        
+    screen.blit(graphic, (screen.get_width()/2-graphic.get_width()/2+offset[0], 
+                          screen.get_height()/2-graphic.get_height()/2+offset[1]))
+    
     
 T = 100
 H = 1
 LEVEL = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,T,T,T,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,T,T,T,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,H,H,H,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,T,T,T,H,H,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,H,H,H,H,H,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,T,T,T,T,T,T,T,T,T,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,H,0,H,0,H,0,H,0,H,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,H,0,H,0,H,0,H,0,H,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,H,0,H,0,H,0,H,0,H,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,H,0,H,0,H,0,H,0,H,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,H,0,H,0,H,0,H,0,H,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,T,T,T,T,T,T,T,T,0,T,T,T,0,T,T,T],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,T,T,T,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,T,T,T,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,H,H,H,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,T,T,T,H,H,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,H,H,H,H,H,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,T,T,T,T,T,T,T,T,T,0,0,0,0,0,0,T,T,T,T,T,T],
+    [0,0,0,0,0,0,H,0,H,0,H,0,H,0,H,0,0,0,0,0,0,H,H,H,H,H,H],
+    [0,0,0,0,0,0,H,0,H,0,H,0,H,0,H,0,0,0,0,0,0,H,0,H,0,H,0],
+    [0,0,0,0,0,0,H,0,H,0,H,0,H,0,H,0,0,0,0,0,0,H,0,H,0,H,0],
+    [0,0,0,0,0,0,H,0,H,0,H,0,H,0,H,0,0,0,0,0,0,H,0,H,0,H,0],
+    [0,0,0,0,0,0,H,0,H,0,H,0,H,0,H,0,0,0,0,0,0,H,0,H,0,H,0],
 ]
 TILE_SIZE = 50
 GRAVITY = 0.25
@@ -77,59 +94,121 @@ JUMP_STRENGTH = 8.25
 MOVE_SPEED = 3.5
 FIRST_COLLIDABLE_TILE = 100
 
+STATE_TITLE = 0
+STATE_GAME = 1
+STATE_FAILED = 2
+
 pygame.init()
 screen = pygame.display.set_mode((800,600))
 clock = pygame.time.Clock()
 
-G_SAM = pygame.image.load("sam.png")
+G_TITLE = pygame.image.load("title.png")
+G_SAM_IDLE = pygame.image.load("sam-idle.png")
+G_SAM_RUN = [ pygame.image.load("sam-run1.png"), pygame.image.load("sam-run2.png") ]
+G_SAM_JUMP = pygame.image.load("sam-jump.png")
 G_BACKGROUND = pygame.image.load("background.png")
 G_TILES = {
     100: pygame.image.load("platform.png"),
     1: pygame.image.load("support.png"),
 }
 
-position = [5*TILE_SIZE, 13*TILE_SIZE]
+F_TEXT = pygame.font.Font("komika.ttf", 72)
+
+state = STATE_TITLE
+position = [0.0,0.0]
 velocity = [0.0,0.0]
 landed = False
 
 while True:
 
+    escape_pressed = False
+    space_pressed = False
     for event in pygame.event.get():
-        if event.type == pygame.locals.QUIT \
-                or (event.type == pygame.locals.KEYUP and event.key == pygame.locals.K_ESCAPE) :
+        if event.type == pygame.locals.QUIT:
             sys.exit()
-        if event.type == pygame.locals.KEYDOWN and event.key == pygame.locals.K_SPACE and landed:
+        if event.type == pygame.locals.KEYDOWN:
+            if event.key == pygame.locals.K_ESCAPE:
+                escape_pressed = True
+            elif event.key == pygame.locals.K_SPACE:
+                space_pressed = True
+
+    if state == STATE_TITLE: 
+
+        if escape_pressed:
+            sys.exit()
+            
+        if space_pressed:
+            position = [8*TILE_SIZE, 13*TILE_SIZE]
+            velocity = [0.0,0.0]
+            landed = False
+            state = STATE_GAME
+            
+        screen.blit(G_TITLE, (0,0))
+        
+        press_space_text = F_TEXT.render("Press space to start", True, (255,0,0))
+        screen.blit(press_space_text, (screen.get_width()/2-press_space_text.get_width()/2,
+                                       screen.get_height()/6*5-press_space_text.get_height()/2))
+    
+    elif state == STATE_GAME: 
+
+        if escape_pressed:
+            state = STATE_TITLE
+
+        if pygame.key.get_pressed()[pygame.locals.K_LEFT]:
+            velocity[0] = -MOVE_SPEED
+        elif pygame.key.get_pressed()[pygame.locals.K_RIGHT]:
+            velocity[0] = MOVE_SPEED
+        else:
+            velocity[0] = 0.0
+            
+        if space_pressed and landed:
             velocity[1] = -JUMP_STRENGTH
             landed = False
-
-    if pygame.key.get_pressed()[pygame.locals.K_LEFT]:
-        velocity[0] = -MOVE_SPEED
-    elif pygame.key.get_pressed()[pygame.locals.K_RIGHT]:
-        velocity[0] = MOVE_SPEED
-    else:
-        velocity[0] = 0.0
-
-    tile_before = tile_ref(position, TILE_SIZE)
-    velocity[1] += GRAVITY
-    position[0] += velocity[0]
-    position[1] += velocity[1]
-    tile_after = tile_ref(position, TILE_SIZE)
-    tile_after_left = tile_ref((position[0]-G_SAM.get_width()/2, position[1]), TILE_SIZE)
-    tile_after_right = tile_ref((position[0]+G_SAM.get_width()/2, position[1]), TILE_SIZE)
     
-    if tile_after[1] > tile_before[1] \
-            and (tile_type(tile_after_left, LEVEL) >= FIRST_COLLIDABLE_TILE \
-              or tile_type(tile_after_right, LEVEL) >= FIRST_COLLIDABLE_TILE):
-        velocity[1] = 0.0
-        position[1] = tile_position(tile_after, TILE_SIZE)[1] - 0.001
-        landed = True
-    else:
-        landed = False
-
-    cam_position = [position[0], position[1]-G_SAM.get_height()]
-    draw_background(screen, G_BACKGROUND, cam_position, (len(LEVEL[0]), len(LEVEL)), TILE_SIZE)
-    draw_tiles(screen, cam_position, LEVEL, G_TILES, TILE_SIZE)
-    screen.blit(G_SAM, (screen.get_width()/2-G_SAM.get_width()/2,screen.get_height()/2))
-
+        tile_before = tile_ref(position, TILE_SIZE)
+        velocity[1] += GRAVITY
+        position[0] += velocity[0]
+        position[1] += velocity[1]
+        tile_after = tile_ref(position, TILE_SIZE)
+        tile_after_left = tile_ref((position[0]-G_SAM_IDLE.get_width()/2, position[1]), TILE_SIZE)
+        tile_after_right = tile_ref((position[0]+G_SAM_IDLE.get_width()/2, position[1]), TILE_SIZE)
+        
+        if tile_after[1] > tile_before[1] \
+                and (tile_type(tile_after_left, LEVEL) >= FIRST_COLLIDABLE_TILE
+                  or tile_type(tile_after_right, LEVEL) >= FIRST_COLLIDABLE_TILE):
+            velocity[1] = 0.0
+            position[1] = tile_position(tile_after, TILE_SIZE)[1] - 0.001
+            landed = True
+        else:
+            landed = False
+            
+        if position[1] >= len(LEVEL)*TILE_SIZE:
+            state = STATE_FAILED
+    
+        cam_position = [position[0], position[1]-G_SAM_IDLE.get_height()]
+        draw_background(screen, G_BACKGROUND, cam_position, (len(LEVEL[0]), len(LEVEL)), TILE_SIZE)
+        draw_tiles(screen, cam_position, LEVEL, G_TILES, TILE_SIZE)
+        draw_sam(screen, G_SAM_IDLE, G_SAM_RUN, G_SAM_JUMP, (0, G_SAM_IDLE.get_height()/2), position, velocity, landed, 
+                 TILE_SIZE)
+                 
+    elif state == STATE_FAILED: 
+        
+        if space_pressed or escape_pressed:
+            state = STATE_TITLE
+            
+        cam_position = [position[0], position[1]-G_SAM_IDLE.get_height()]
+        draw_background(screen, G_BACKGROUND, cam_position, (len(LEVEL[0]), len(LEVEL)), TILE_SIZE)
+        draw_tiles(screen, cam_position, LEVEL, G_TILES, TILE_SIZE)
+        draw_sam(screen, G_SAM_IDLE, G_SAM_RUN, G_SAM_JUMP, (0, G_SAM_IDLE.get_height()/2), position, velocity, landed, 
+                 TILE_SIZE)
+                 
+        gameover_text = F_TEXT.render("Whoops!", True, (255,0,0))
+        screen.blit(gameover_text, (screen.get_width()/2-gameover_text.get_width()/2, 
+                                    screen.get_height()/3-gameover_text.get_height()/2))
+                                    
+        press_space_text = F_TEXT.render("Press space", True, (255,0,0))
+        screen.blit(press_space_text, (screen.get_width()/2-press_space_text.get_width()/2,
+                                       screen.get_height()/6*5-press_space_text.get_height()/2))
+                 
     pygame.display.flip()
     clock.tick(60)
