d450ecd4e683ae9a948f21218c60f705900f0612
mark <mfrimston@gmail.com>
2015-05-02 18:19:43 +0100

    added more code comments

--- main.py
+++ main.py
@@ -154,42 +154,60 @@ def draw_sam(screen, idle_graphic, run_graphics, jump_graphic, fall_graphic, off
        velocity is the current x and y speed of the player as a 2-item sequence
        landed is a boolean indicating whether sam is on a platform or not
        tile_size is the size of each tile, as an integer"""
-    # if sam is in mid-air 
+    # if sam is in mid-air and moving upwards, show jumping graphic
     if not landed and velocity[1] <= 0:
         graphic = jump_graphic
-        
+    # if sam is in mid-air and moving downwards, show falling graphic
     elif not landed and velocity[1] > 0:
         graphic = fall_graphic
+    # if sam is on the ground and not moving left or right, show idle graphic
     elif velocity[0] == 0.0:
         graphic = idle_graphic
+    # if sam is on the ground and moving left or right, show a running graphic. The graphic shown is based on sam's x
+    # coordinate so that as she moves faster, the graphics animate faster.
     else:
         num = len(run_graphics)
         graphic = run_graphics[int(position[0]//((tile_size*4)/num)) % num]
         
+    # if sam is moving left, flip the graphic so that she is facing left instead of right
     if velocity[0] < 0.0:
         graphic = pygame.transform.flip(graphic, True, False)
         
+    # draw the selected graphic to the screen. It's always drawn to the same place on the screen, just below the centre.
     screen.blit(graphic, (screen.get_width()/2-graphic.get_width()/2+offset[0], 
                           screen.get_height()/2-graphic.get_height()/2+offset[1]))
     
     
 def find_position(tile_type, level, tile_size):
+    """Finds the first occurence of the given tile type found in the level, and returns the x,y coordinates of its
+    centre as a 2-tuple. If not found, returns 0,0"""
+    # iterate through all the level tiles
     for j, row in enumerate(level):
         for i, tile in enumerate(row):
+            # if this tile is the desired type, return its centre coordinates
             if tile == tile_type:
                 return (i+0.5)*tile_size, (j+0.5)*tile_size             
+    # if we got this far, the tile type wasn't found. Return default of 0,0
     return 0.0, 0.0
 
 
 ##### CONSTANTS ########################################################################################################
     
     
+# These constants are just a handy way of refering to some of the special tile types.
 TILE_START = 1
 TILE_FINISH = 25
 TILE_GROUND = 4
 TILE_SKY = 0
 
 def define_level():
+    """Returns the level definition as a nested list; each item in the list is a row, and each item in a row is a
+       column refering to a numerical tile type"""
+       
+    # We temporarily (just within the scope of this function) define these one-letter variables to refer to each tile
+    # type a convenient and compact way of laying out the level definition in the nested list below.
+    # This is a pretty cheap and cheerful way of defining the level, as it makes it awkward to add further tile types.
+    # To extend the tile types further, you might want to consider using a tile-editor program such as Tiled.
     S = TILE_START
     N = 2 # concrete
     Z = 3 # concreate platform
@@ -216,6 +234,7 @@ def define_level():
     U = 24 # sign up
     F = TILE_FINISH
 
+    # The level definition
     return [
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -297,21 +316,31 @@ def define_level():
     [0,0,0,0,0,0,0,0,0,0,W,W,W,W,H,H,W,W,W,W,W,W,W,W,H,H,W,W,W,W,W,W,W,W,W,H,H,W,W,W,W,W,W,W,W,H,H,0,0,0,0,0,0,0,0,0,0],
     ]
 
+# Call the function above and store the result in LEVEL
 LEVEL = define_level()
 
-# Constants defining various aspects of Sam's movement
-SCREEN_SIZE = 1024, 768
-TILE_SIZE = 64              # The size of each tile
+SCREEN_SIZE = 1024, 768     # The screen resolution the game runs at.
+TILE_SIZE = 64              # The size of each tile (they're square so width and height use the same value)
+
+# These constants define various aspects of Sam's movement. They were fine-tuned by trial and error.
 GRAVITY = 0.64              # The rate at which Sam accellerates downwards due to gravity
 JUMP_STRENGTH = 15.2        # Higher jump strength means Sam will jump higher
 ACCELERATION = 0.26         # The rate that Sam speeds up to full running speed
 DEACCELERATION = 0.26       # The rate that Sam skids to a stop after running
 MAX_SPEED = 6.46            # Sam's maximum running speed
-SAM_SIZE = 64
-FEET_WIDTH = 38             # the width of the bit of Sam that sits on the platform
-COLLIDABLE_TILES = {3,7,10,16,18,20}
 
-# Constants for the different states the game can be in
+SAM_SIZE = 64               # The size of Sam's graphics
+FEET_WIDTH = 38             # The width of the bit of Sam that sits on the platform:
+                            #       |          |
+                            #       |  feet w  | Sam
+                            #       |  /    \  | 
+                            #       +-+------+-+
+                            #-----------+
+                            # Platform  |
+                            #           |
+COLLIDABLE_TILES = {3,7,10,16,18,20}  # The tile types that Sam can stand on (platforms)
+
+# Constants for the different states the game can be in (see below)
 STATE_TITLE = 0
 STATE_HELP = 1
 STATE_GAME = 2
@@ -321,15 +350,22 @@ STATE_WON = 4
 
 #### MAIN PROGRAM ######################################################################################################
 
-
+# Initialise Pygame
 pygame.init()
-screen = pygame.display.set_mode(SCREEN_SIZE,pygame.locals.DOUBLEBUF|pygame.locals.HWSURFACE)
+# Set the screen resolution and store the screen surface for drawing on
+screen = pygame.display.set_mode(SCREEN_SIZE,pygame.locals.DOUBLEBUF|pygame.locals.HWSURFACE|pygame.locals.FULLSCREEN)
+# Hide the mouse cursor
 pygame.mouse.set_visible(False)
+# Create a clock object for maintaining a consistent framerate
 clock = pygame.time.Clock()
+# Create an offscreen surface for more efficient tile drawing (see "draw_tiles" function)
 tile_buffer = pygame.Surface(SCREEN_SIZE,flags=pygame.locals.SRCALPHA)
 
-G_TITLE = pygame.transform.scale(pygame.image.load("title.png"),SCREEN_SIZE).convert(screen)
-G_HELP = pygame.transform.scale(pygame.image.load("help.png"),SCREEN_SIZE)
+# Load graphics and store them in G_* variables. We call "convert" on our opaque background graphics to make pygame
+# convert them to an optimal format for the screen buffer, which disables per-pixel alpha on them but makes them faster
+# to draw
+G_TITLE = pygame.image.load("title.png").convert(screen)
+G_HELP = pygame.image.load("help.png").convert(screen)
 G_SAM_IDLE = pygame.image.load("sam-idle.png")
 G_SAM_RUN = [ pygame.image.load("sam-run1.png"), 
               pygame.image.load("sam-run2.png"), 
@@ -337,117 +373,164 @@ G_SAM_RUN = [ pygame.image.load("sam-run1.png"),
               pygame.image.load("sam-run4.png"), ]
 G_SAM_JUMP = pygame.image.load("sam-jump.png")
 G_SAM_FALL = pygame.image.load("sam-fall.png")
-G_SAM_WIN = pygame.transform.scale(pygame.image.load("sam-win.png"),(64,64))
-G_SAM_FAIL = pygame.transform.scale(pygame.image.load("sam-fail.png"),(64,64))
+G_SAM_WIN = pygame.image.load("sam-win.png")
+G_SAM_FAIL = pygame.image.load("sam-fail.png")
 G_BACKGROUND = pygame.image.load("background.png").convert(screen)
+# load the tile graphics, each of which is named according to its numerical type. Type 0 is empty space and type 1 is
+# the invisible start position. Neither of these have a graphic so we put None in the list for each of these.
 G_TILES = [None, None]
 for i in range(2,26):
     G_TILES.append(pygame.image.load("tile{:0>2}.png".format(i)))
 
+# Load the fonts
 F_BIG = pygame.font.Font("komika.ttf", 72)
 F_SMALL = pygame.font.Font("komika.ttf", 36)
 
+# Find the start and finish tiles in the level definition and store their x,y coordinates in these constants
 START_POSITION = find_position(TILE_START, LEVEL, TILE_SIZE)
 FINISH_POSITION = find_position(TILE_FINISH, LEVEL, TILE_SIZE)
 
-state = STATE_TITLE
-position = [0.0,0.0]
-velocity = [0.0,0.0]
-landed = False
-last_cam_position = [0.0,0.0]
+state = STATE_TITLE   # The game's state - start on the title screen
+position = [0.0,0.0]  # Sam's x,y coordinates
+velocity = [0.0,0.0]  # Sam's x and y speed. These are added to Sam's position each frame to move her
+landed = False        # Stores whether Sam is on a platform or not
+last_cam_position = [0.0,0.0]  # Stores the position the "camera" was at for the previous frame. The camera is the 
+                               # imaginary object that the viewport is centered on, just above Sam's head
 
+# The main game loop. It loops until the player quits
 while True:
 
+    # variables to store whether the escape and space keys became pressed this frame or not. We only want to react to
+    # these keys' initial pressed event - we don't care if they're held down.
     escape_pressed = False
     space_pressed = False
+    # loop over the system events collected by Pygame
     for event in pygame.event.get():
+        # if the player closed the window, quit
         if event.type == pygame.locals.QUIT:
             sys.exit()
+        # if the escape or space (or anothe jump button) was pressed down this frame, set the corresponding variable
         if event.type == pygame.locals.KEYDOWN:
             if event.key == pygame.locals.K_ESCAPE:
                 escape_pressed = True
             elif event.key in (pygame.locals.K_SPACE, pygame.locals.K_UP, pygame.locals.K_w):
                 space_pressed = True
 
-    if state == STATE_TITLE: 
+    # The title screen
+    if state == STATE_TITLE:  # ----------------------------------------------------------------------------------------
 
+        # if player hits escape, quit the game
         if escape_pressed:
             sys.exit()
             
+        # if player hits the jump button, proceed to the "how to play" screen
         if space_pressed:
             state = STATE_HELP
             
+        # draw the title screen graphic to the screen surface
         screen.blit(G_TITLE, (0,0))
         
         
-    elif state == STATE_HELP:
+    # The "how to play" screen
+    elif state == STATE_HELP:  #----------------------------------------------------------------------------------------
     
+        # if the player hits the jump button or the escape key, set up the game and proceed to the main game state
         if space_pressed or escape_pressed:
             position = list(START_POSITION)
             velocity = [0.0,0.0]
             landed = False
             state = STATE_GAME
             
+        # draw the "how to play" screen graphic to the screen surface
         screen.blit(G_HELP, (0,0))
         
         
-    elif state == STATE_GAME: 
+    # The main game 
+    elif state == STATE_GAME:  # ---------------------------------------------------------------------------------------
 
+        # if the player hits the escape key, go back to the title screen
         if escape_pressed:
             state = STATE_TITLE
 
+        # if the player is holding down one of the "left" keys (left cursor or A), accelerate Sam left
         if pygame.key.get_pressed()[pygame.locals.K_LEFT] or pygame.key.get_pressed()[pygame.locals.K_a]:
+            # only accelerate if under max speed
             if velocity[0] > -MAX_SPEED:
                 velocity[0] -= ACCELERATION
+        # if the player is holding down one of the "right" keys (right cursor or D), accelerate Sam right
         elif pygame.key.get_pressed()[pygame.locals.K_RIGHT] or pygame.key.get_pressed()[pygame.locals.K_d]:
+            # only accelerate if under max speed
             if velocity[0] < MAX_SPEED:
                 velocity[0] += ACCELERATION
+        # if the player isn't holding left or right, deacelerate Sam so she eventually comes to a halt
         else:
+            # if moving right and subtracting DEACCELERATION wouldn't push speed past zero, deacelerate
             if velocity[0] > 0 and velocity[0]-DEACCELERATION >= 0:
                 velocity[0] -= DEACCELERATION
+            # if moving left and adding DEACCELERATION wouldn't push speed past zero, deacelerate
             elif velocity[0] < 0 and velocity[0]+DEACCELERATION <= 0:
                 velocity[0] += DEACCELERATION
+            # moving slower than DEACCELERATION, so just reduce speed to zero
             else:
                 velocity[0] = 0.0
             
+        # if player hits a jump button and Sam is on a platform, make Sam jump by setting her vertical speed
         if space_pressed and landed:
             velocity[1] = -JUMP_STRENGTH
             landed = False
     
+        # store the column and row of the tile Sam was at before moving - this is used later
         tile_before = tile_ref(position, TILE_SIZE)
+        # accelerate Sam downwards to simulate gravity, by increasing vertical speed
         velocity[1] += GRAVITY
+        # move Sam by her velocity
         position[0] += velocity[0]
         position[1] += velocity[1]
+        # store the column and row of the tile Sam has just moved to (at the middle of her feet)
         tile_after = tile_ref(position, TILE_SIZE)
+        # also store column and row of the tiles at the left and right edges of Sam's feet
         tile_after_left = tile_ref((position[0]-FEET_WIDTH/2, position[1]), TILE_SIZE)
         tile_after_right = tile_ref((position[0]+FEET_WIDTH/2, position[1]), TILE_SIZE)
         
+        # If Sam's position changed to a lower tile, and it was a collidable tile (i.e. she fell down into a platform).
+        # This will happen every frame while Sam is standing on a platform.
         if tile_after[1] > tile_before[1] \
                 and (tile_type(tile_after_left, LEVEL, 0, 0) in COLLIDABLE_TILES
                   or tile_type(tile_after_right, LEVEL, 0, 0) in COLLIDABLE_TILES):
+            # Stop Sam moving vertically and position her just above the platform she fell into
             velocity[1] = 0.0
             position[1] = tile_position(tile_after, TILE_SIZE)[1] - 0.001
             landed = True
+        # Otherwise, Sam isn't standing on a platform - she's in mid-air.
         else:
             landed = False
             
+        # If Sam has fallen past the bottom of the level, go to the "game over" screen
         if position[1] >= len(LEVEL)*TILE_SIZE:
             state = STATE_FAILED
             
+        # If Sam is on the finish tile, go to the "congratulations" screen
         if FINISH_POSITION[0]-TILE_SIZE/2 < position[0] < FINISH_POSITION[0]+TILE_SIZE/2 \
                 and FINISH_POSITION[1]-TILE_SIZE/2 < position[1] < FINISH_POSITION[1]+TILE_SIZE/2:
             state = STATE_WON
     
+        # The camera is the imaginary object that the viewport is centered on. Store it's position as just above Sam's
+        # head, so the player sees slightly more of the level above Sam than below
         cam_position = [position[0], position[1]-SAM_SIZE]
+        # Draw the background graphic
         draw_background(screen, G_BACKGROUND, cam_position, (len(LEVEL[0]), len(LEVEL)), TILE_SIZE)
+        # Draw the tiles on top of the background
         draw_tiles(screen, tile_buffer, last_cam_position, cam_position, LEVEL, G_TILES, TILE_GROUND, 
                    TILE_SKY, TILE_SIZE)
+        # Finally draw Sam on top of the tiles
         draw_sam(screen, G_SAM_IDLE, G_SAM_RUN, G_SAM_JUMP, G_SAM_FALL, (0, SAM_SIZE/2), position, velocity, landed, 
                  TILE_SIZE)
         
+        # Update the last camera position so we can check how far the camera moved next frame
         last_cam_position = cam_position
                  
-    elif state == STATE_FAILED: 
+    # The "game over" screen
+    elif state == STATE_FAILED:  # ------------------------------------------------------------------------------------ 
         
         if space_pressed or escape_pressed:
             state = STATE_TITLE
